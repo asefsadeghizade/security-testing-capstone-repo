e47b819b7286549d2368583446bfc97dc7daf73a
asefsadeghizade <asefsadeghi@gmail.com>
2024-07-30 17:33:52 +0330

    basic auth added

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("menu-items/", views.MenuItemView.as_view(), name="menu-items"),
     path("menu-items/<int:pk>", views.SingleMenuItemView.as_view(), name="single-menu-item"),
+    path("message/", views.msg, name="auth-msg"),
 ]
